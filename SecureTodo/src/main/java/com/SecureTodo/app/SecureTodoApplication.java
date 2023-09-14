9a159240facf1522be72a8485d3b971cd912bbbd
whwnd <whwnd5993@naver.com>
2023-09-14 12:03:21 +0900

    first commit

---
+++ SecureTodo/src/main/java/com/SecureTodo/app/SecureTodoApplication.java
@@ -0,0 +1,13 @@
+package com.SecureTodo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTodoApplication.class, args);
+	}
+
+}
