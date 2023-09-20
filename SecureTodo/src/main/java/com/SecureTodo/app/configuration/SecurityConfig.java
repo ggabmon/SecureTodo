dad94f4109d85eb992094e33a967dbd2b6b0ed2e
whwnd <whwnd5993@naver.com>
2023-09-20 17:52:48 +0900

    secure 제작 재시도중

--- SecureTodo/src/main/java/com/SecureTodo/app/configuration/SecurityConfig.java
+++ SecureTodo/src/main/java/com/SecureTodo/app/configuration/SecurityConfig.java
@@ -7,24 +7,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration{
+public class SecurityConfig{ 
+
+	// extends WebSecurityConfigurerAdapter {
 	
 	/*
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http
-        .csrf().disable()
-        .authorizeRequests()
-            .antMatchers("/login**").permitAll() // 로그인 경로는 모든 사람이 접근 가능
-            .anyRequest().authenticated() // 그 외 경로는 인증된 사용자만 접근 가능
-        .and()
-        .formLogin()
-            .loginPage("/login") // 로그인 페이지 URL
-            .defaultSuccessURL("/todo", true) // 로그인 성공 시 리다이렉트할 URL
-        .and()
-        .logout()
-            .logoutUrl("/logout") // 로그아웃 URL
-            .logoutSuccessUrl("/login"); // 로그아웃 성공 시 리다이렉트할 URL
-	}
-	*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .requestMatchers(PathRequest.to("/login**")).permitAll() // 로그인 경로는 모든 사람이 접근 가능
+                .anyRequest().authenticated() // 그 외 경로는 인증된 사용자만 접근 가능
+            .and()
+            .formLogin()
+                .loginPage("/login") // 로그인 페이지 URL
+                .defaultSuccessURL("/todo", true) // 로그인 성공 시 리다이렉트할 URL
+            .and()
+            .logout()
+                .logoutUrl("/logout") // 로그아웃 URL
+                .logoutSuccessUrl("/login"); // 로그아웃 성공 시 리다이렉트할 URL
+    }
+    */
 }
+
